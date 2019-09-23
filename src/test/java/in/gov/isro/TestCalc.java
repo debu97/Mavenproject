2a8ce7c954e4b0069beb40f1be4fa00e18a36b6d
debu97 <debu97@gmail.com>
2019-09-23 12:06:21 +0530

    Subtract method added

--- src/test/java/in/gov/isro/TestCalc.java
+++ src/test/java/in/gov/isro/TestCalc.java
@@ -18,4 +18,10 @@ public class TestCalc {
 		int result = calc.multiply(5,5);
 		assertEquals(25,result);
 	}
+	@Test
+	public void testsubtract() {
+		Calculator calc = new Calculator();
+		int result = calc.subtract(10,5);
+		assertEquals(5,result);
+	}
 }
